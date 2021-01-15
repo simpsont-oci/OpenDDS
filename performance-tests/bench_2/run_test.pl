f8bac41f4bfb8d298350eb5037cc2b65b7efddf2
Timothy Simpson <simpsont@objectcomputing.com>
2021-01-15 10:38:39 -0600

    testing workflows, need still yet even another further commit

--- performance-tests/bench_2/run_test.pl
+++ performance-tests/bench_2/run_test.pl
@@ -41,6 +41,10 @@ if ($test->flag('flisco')) {
   $tc_opts .= " ci_disco --json";
   $is_rtps_disc = 1;
 }
+if ($test->flag('grisco')) {
+  $tc_opts .= " ci_disco";
+  $is_rtps_disc = 1;
+}
 elsif ($test->flag('fan')) {
   $tc_opts .= " ci_fan --override-start-time 15 --show-worker-logs";
   $is_rtps_disc = 1;
