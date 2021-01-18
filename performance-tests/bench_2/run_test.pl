fc7b11dce33bc85ac7e32b41bf36760e1758bb72
Timothy Simpson <simpsont@objectcomputing.com>
2021-01-18 12:26:45 -0600

    update #02

--- performance-tests/bench_2/run_test.pl
+++ performance-tests/bench_2/run_test.pl
@@ -33,7 +33,7 @@ if ($test->flag('disco')) {
   $tc_opts .= " ci_disco --show-worker-logs";
   $is_rtps_disc = 1;
 }
-if ($test->flag('lisco')) {
+if ($test->flag('crisco')) {
   $tc_opts .= " ci_disco --show-worker-logs --json";
   $is_rtps_disc = 1;
 }
