8e136373c0b7572126d66fa729471aabf045c09c
Timothy Simpson <simpsont@objectcomputing.com>
2021-01-20 10:35:37 -0600

    update 14

--- performance-tests/bench_2/run_test.pl
+++ performance-tests/bench_2/run_test.pl
@@ -33,7 +33,7 @@ if ($test->flag('disco')) {
   $tc_opts .= " ci_disco --show-worker-logs";
   $is_rtps_disc = 1;
 }
-if ($test->flag('visco')) {
+if ($test->flag('wisco')) {
   $tc_opts .= " ci_disco --show-worker-logs --json";
   $is_rtps_disc = 1;
 }
