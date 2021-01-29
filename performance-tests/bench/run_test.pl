1a4b56b6d44d79213184b058565f34be9f6e5149
Timothy Simpson <simpsont@objectcomputing.com>
2021-01-29 13:25:59 -0600

    Update 01

--- performance-tests/bench/run_test.pl
+++ performance-tests/bench/run_test.pl
@@ -48,7 +48,7 @@ if ($test->flag('disco')) {
   $tc_opts .= " ci_disco";
   $is_rtps_disc = 1;
 }
-elsif ($test->flag('stan')) {
+elsif ($test->flag('fan_01')) {
   $tc_opts .= " ci_fan";
   $is_rtps_disc = 1;
 }
